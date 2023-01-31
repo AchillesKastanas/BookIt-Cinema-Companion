8b23c494fda94d545295ef3eb8d80044d80fbc69
AchillesKastanas <achilleasrinos@gmail.com>
2023-01-31 12:02:12 +0200

    added dynamic switching between reservations and history

--- src/pages/BookingPage.jsx
+++ src/pages/BookingPage.jsx
@@ -11,11 +11,30 @@ const BookingPage = () => {
 	const [data, setData] = useState([]);
 	const [selectedRoom, setSelectedRoom] = useState(0);
 	const [selectedSeats, setSelectedSeats] = useState([]);
+	const [selectedDate, setSelectedDate] = useState(new Date());
+	const [showHistory, setShowHistory] = useState(false);
 
 	useEffect(() => {
 		// When the selectedSeats updates, update the cart
-		console.log("useEffect", selectedSeats);
+		//console.log("useEffect", selectedSeats);
 	}, [selectedSeats]);
+
+	//Manage requests based on date + manage ui
+	useEffect(() => {
+		// When selectedDate updates
+		const today = new Date();
+		//Check if the selected date is today
+		if (selectedDate <= today) {
+			if (selectedDate.getDate() === today.getDate()) {
+				setShowHistory(false);
+			} else {
+				setShowHistory(true);
+				setSelectedSeats([]);
+			}
+		} else {
+			setShowHistory(false);
+		}
+	}, [selectedDate]);
 
 	const addToSelectedSeats = (seatData) => {
 		!selectedSeats.includes(seatData) &&
@@ -107,6 +126,11 @@ const BookingPage = () => {
 		// setSelectedRoom(resData.rooms[0]);
 	}, []);
 
+	//Check if date is older than today. Compare only the date, not the time
+	const checkDate = (date) => {
+		setSelectedDate(date);
+	};
+
 	return (
 		<div className={classes.generalContainer}>
 			<p className={classes.titleBig}>[movie_title]</p>
@@ -118,89 +142,212 @@ const BookingPage = () => {
 						<br />
 						<Calendar
 							className={classes.calendar}
+							value={selectedDate}
 							oneWeekCalendar={false}
+							onChange={checkDate}
 						></Calendar>
 					</div>
 				</ColoredContainer>
 				<ColoredContainer className="colored_container__blue__reserve_center">
 					<div className={classes.verticalContainer}>
-						<p className={classes.titleBig}>
-							Where would you like to sit?
-						</p>
-						<br />
-						<br />
-						<div className={classes.horizontalContainer}>
-							{/* Foreach room in resData create a button that loads the corresponding room*/}
-							{data &&
-								data.map((room, index) =>
-									index === selectedRoom ? (
-										<Button
-											key={index}
-											value={`Room ${index + 1}`}
-											className="roomButtonActive"
-											onClick={() => {
-												setSelectedRoom(index);
-											}}
-										/>
-									) : (
-										<Button
-											key={index}
-											value={`Room ${index + 1}`}
-											className="roomButton"
-											onClick={() => {
-												setSelectedRoom(index);
-											}}
-										/>
-									)
-								)}
-						</div>
-						<p className={classes.titleBig}>Available seats:</p>
-						<div className={classes.horizontalContainerCentered}>
-							<SeatGrid
-								room={
-									typeof data[selectedRoom] !== "undefined" &&
-									data[selectedRoom]
-								}
-								addToSelectedSeats={addToSelectedSeats}
-							/>
-						</div>
+						{!showHistory ? (
+							<>
+								<p className={classes.titleBig}>
+									Where would you like to sit?
+								</p>
+								<br />
+								<br />
+								<div className={classes.horizontalContainer}>
+									{/* Foreach room in resData create a button that loads the corresponding room*/}
+									{data &&
+										data.map((room, index) =>
+											index === selectedRoom ? (
+												<Button
+													key={index}
+													value={`Room ${index + 1}`}
+													className="roomButtonActive"
+													onClick={() => {
+														setSelectedRoom(index);
+													}}
+												/>
+											) : (
+												<Button
+													key={index}
+													value={`Room ${index + 1}`}
+													className="roomButton"
+													onClick={() => {
+														setSelectedRoom(index);
+													}}
+												/>
+											)
+										)}
+								</div>
+								<p className={classes.titleBig}>
+									Available seats:
+								</p>
+								<div
+									className={
+										classes.horizontalContainerCentered
+									}
+								>
+									<SeatGrid
+										room={
+											typeof data[selectedRoom] !==
+												"undefined" &&
+											data[selectedRoom]
+										}
+										addToSelectedSeats={addToSelectedSeats}
+									/>
+								</div>
+							</>
+						) : (
+							<>
+								<p className={classes.titleBig}>
+									Reservations History
+								</p>
+								<br />
+								<br />
+								<div className={classes.horizontalContainer}>
+									{/* Foreach room in resData create a button that loads the corresponding room*/}
+									{data &&
+										data.map((room, index) =>
+											index === selectedRoom ? (
+												<Button
+													key={index}
+													value={`Room ${index + 1}`}
+													className="roomButtonActive"
+													onClick={() => {
+														setSelectedRoom(index);
+													}}
+												/>
+											) : (
+												<Button
+													key={index}
+													value={`Room ${index + 1}`}
+													className="roomButton"
+													onClick={() => {
+														setSelectedRoom(index);
+													}}
+												/>
+											)
+										)}
+								</div>
+								<p className={classes.titleBig}>
+									Available seats:
+								</p>
+								<div
+									className={
+										classes.horizontalContainerCentered
+									}
+								>
+									<SeatGrid
+										room={
+											typeof data[selectedRoom] !==
+												"undefined" &&
+											data[selectedRoom]
+										}
+									/>
+								</div>
+							</>
+						)}
 					</div>
 				</ColoredContainer>
 				<ColoredContainer className="colored_container__blue__reserve_side">
 					<div className={classes.verticalContainer}>
-						<p className={classes.titleBig}>Your Cart</p>
-						<br />
-						<div className={classes.cartItems}>
-							{selectedSeats
-								.slice(0)
-								.reverse()
-								.map((seatData, index) => {
-									return (
-										<ColoredContainer
-											key={index}
-											className="colored_container__light_blue"
-										>
-											<p className={classes.titleMid}>
-												Room: {seatData[0]}
-											</p>
-											<p className={classes.titleSmall}>
-												Seat: {seatData[1]} - Cost: 15$
-											</p>
-											<Button
-												value="Remove"
-												className="removeButton"
-												onClick={() => {
-													removeFromSelectedSeats(
-														seatData
-													);
-												}}
-											/>
-										</ColoredContainer>
-									);
-								})}
-						</div>
-						{selectedSeats.length !== 0 && (
-							<Button value="Order Now" className="orderButton" />
+						{!showHistory ? (
+							<>
+								<p className={classes.titleBig}>Your Cart</p>
+								<br />
+								<div className={classes.cartItems}>
+									{selectedSeats
+										.slice(0)
+										.reverse()
+										.map((seatData, index) => {
+											return (
+												<ColoredContainer
+													key={index}
+													className="colored_container__light_blue"
+												>
+													<p
+														className={
+															classes.titleMid
+														}
+													>
+														Room: {seatData[0]}
+													</p>
+													<p
+														className={
+															classes.titleSmall
+														}
+													>
+														Seat: {seatData[1]} -
+														Cost: 15$
+													</p>
+													<Button
+														value="Remove"
+														className="removeButton"
+														onClick={() => {
+															removeFromSelectedSeats(
+																seatData
+															);
+														}}
+													/>
+												</ColoredContainer>
+											);
+										})}
+								</div>
+								{selectedSeats.length !== 0 && (
+									<Button
+										value="Order Now"
+										className="orderButton"
+									/>
+								)}
+							</>
+						) : (
+							<>
+								<p className={classes.titleBig}>
+									Visitor History
+								</p>
+								<br />
+								<div className={classes.cartItems}>
+									{selectedSeats
+										.slice(0)
+										.reverse()
+										.map((seatData, index) => {
+											return (
+												<ColoredContainer
+													key={index}
+													className="colored_container__light_blue"
+												>
+													<p
+														className={
+															classes.titleMid
+														}
+													>
+														Room: {seatData[0]}
+													</p>
+													<p
+														className={
+															classes.titleSmall
+														}
+													>
+														Seat: {seatData[1]} -
+														Cost: 15$
+													</p>
+													<Button
+														value="Remove"
+														className="removeButton"
+														onClick={() => {
+															removeFromSelectedSeats(
+																seatData
+															);
+														}}
+													/>
+												</ColoredContainer>
+											);
+										})}
+								</div>
+							</>
 						)}
 					</div>
 				</ColoredContainer>
